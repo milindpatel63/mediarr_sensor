730ae1e45a9eb3aec7b477e35a0c7affc11bc5bd
Vansmak <i.am.vansmak@gmail.com>
2025-01-24 20:33:12 -0800

    Add files via upload

--- custom_components/mediarr/sensor.py
+++ custom_components/mediarr/sensor.py
@@ -1,12 +1,14 @@
 # mediarr/sensor.py
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
 from custom_components.mediarr.common.const import (
     CONF_MAX_ITEMS, 
     CONF_DAYS, 
     DEFAULT_MAX_ITEMS, 
-    DEFAULT_DAYS,
-    TRAKT_ENDPOINTS,
-    TMDB_ENDPOINTS
+    DEFAULT_DAYS
 )
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
@@ -40,32 +42,26 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
             config["radarr"].get("max_items", DEFAULT_MAX_ITEMS)
         ))
 
-    # Discovery Sensors - Trakt
+    # Discovery Sensors
     if "trakt" in config:
         from .discovery.trakt import TraktMediarrSensor
-        for endpoint in TRAKT_ENDPOINTS:
-            if config["trakt"].get(endpoint, endpoint == 'trending'):  # trending enabled by default
-                sensors.append(TraktMediarrSensor(
-                    session,
-                    config["trakt"]["client_id"],
-                    config["trakt"]["client_secret"],
-                    endpoint,
-                    config["trakt"].get("media_type", "both"),
-                    config["trakt"].get("max_items", DEFAULT_MAX_ITEMS),
-                    config["trakt"].get("tmdb_api_key")
-                ))
+        sensors.append(TraktMediarrSensor(
+            session,
+            config["trakt"]["client_id"],
+            config["trakt"]["client_secret"],
+            config["trakt"].get("trending_type", "both"),
+            config["trakt"].get("max_items", DEFAULT_MAX_ITEMS),
+            config["trakt"]["tmdb_api_key"]
+        ))
 
-    # Discovery Sensors - TMDB
     if "tmdb" in config:
         from .discovery.tmdb import TMDBMediarrSensor
-        for endpoint in TMDB_ENDPOINTS:
-            if config["tmdb"].get(endpoint, endpoint == 'trending'):  # trending enabled by default
-                sensors.append(TMDBMediarrSensor(
-                    session,
-                    config["tmdb"]["api_key"],
-                    endpoint=endpoint,
-                    max_items=config["tmdb"].get("max_items", DEFAULT_MAX_ITEMS)
-                ))
+        sensors.append(TMDBMediarrSensor(
+            session,
+            config["tmdb"]["api_key"],
+            config["tmdb"].get("max_items", DEFAULT_MAX_ITEMS)
+        ))
 
     if sensors:
         async_add_entities(sensors, True)
+
